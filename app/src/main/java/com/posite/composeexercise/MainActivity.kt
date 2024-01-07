01b2b7e1a94400f7d357fa1b69bc65247d3c8116
posite <su84053@gmail.com>
2024-01-07 22:33:29 +0900

    feat: 간단한 lazycolumn 사용

--- app/src/main/java/com/posite/composeexercise/MainActivity.kt
+++ app/src/main/java/com/posite/composeexercise/MainActivity.kt
@@ -3,80 +3,37 @@ package com.posite.composeexercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
-            }
-            ImageCard(
-                isFavorite = isFavorite,
-            ) { favorite ->
-                isFavorite = favorite
-            }
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    isFavorite: Boolean,
-    onClickFavorite: (Boolean) -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .wrapContentSize(Alignment.TopStart)
-            .padding(16.dp),
-        shape = RoundedCornerShape(8.dp),
-    ) {
-        Box(
-            modifier = Modifier
-                .wrapContentSize()
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.poster),
-                contentDescription = "서울의 봄",
-                contentScale = ContentScale.Crop
-            )
-
-            Box(
-                modifier = Modifier.matchParentSize(),
-                contentAlignment = Alignment.TopEnd
+            LazyColumn(modifier = Modifier
+                .background(color = Color.Yellow),
+                contentPadding = PaddingValues(10.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                IconButton(onClick = {
-                    onClickFavorite(isFavorite.not())
-                }) {
-                    Icon(
-                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = "favorite",
-                        tint = Color.White
-                    )
+                item {
+                    Text("header")
+                }
+                items(50) {index ->
+                    Text("$index 번째")
+                }
+
+                item {
+                    Text("footer")
                 }
             }
         }
     }
 }
+
