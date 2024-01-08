5b82cc42b31505b8d643a1a4737eadd50031d22f
posite <su84053@gmail.com>
2024-01-08 22:49:11 +0900

    feat: 간단한 Snackbar 사용

--- app/src/main/java/com/posite/composeexercise/MainActivity.kt
+++ app/src/main/java/com/posite/composeexercise/MainActivity.kt
@@ -3,36 +3,65 @@ package com.posite.composeexercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(modifier = Modifier
-                .background(color = Color.Yellow),
-                contentPadding = PaddingValues(10.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                item {
-                    Text("header")
-                }
-                items(50) {index ->
-                    Text("$index 번째")
-                }
-
-                item {
-                    Text("footer")
-                }
+            val snackbarHostState = remember { SnackbarHostState() }
+            val scope = rememberCoroutineScope()
+            val (text, setText) = remember{
+                mutableStateOf("")
             }
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+            Scaffold(
+                snackbarHost = { SnackbarHost(snackbarHostState) },
+                content = { innerPadding ->
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        TextField(
+                            value = text,
+                            onValueChange = setText
+                        )
+                        Button(onClick = {
+                            keyboardController?.hide()
+                            scope.launch {
+                                snackbarHostState.showSnackbar(message = text, duration = SnackbarDuration.Short)
+                            }
+                        }) {
+                            Text("클릭!")
+                        }
+
+                    }
+                }
+            )
         }
     }
 }
