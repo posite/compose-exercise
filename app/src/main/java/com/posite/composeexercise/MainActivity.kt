986aeab79fb853efe98275ccda71b33b4ab2e954
posite <su84053@gmail.com>
2024-01-08 23:17:42 +0900

    feat: 간단하게 compose 사용

--- app/src/main/java/com/posite/composeexercise/MainActivity.kt
+++ app/src/main/java/com/posite/composeexercise/MainActivity.kt
@@ -5,63 +5,113 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val snackbarHostState = remember { SnackbarHostState() }
-            val scope = rememberCoroutineScope()
-            val (text, setText) = remember{
-                mutableStateOf("")
-            }
-            val keyboardController = LocalSoftwareKeyboardController.current
-
-            Scaffold(
-                snackbarHost = { SnackbarHost(snackbarHostState) },
-                content = { innerPadding ->
-                    Column(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(innerPadding),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        TextField(
-                            value = text,
-                            onValueChange = setText
-                        )
-                        Button(onClick = {
-                            keyboardController?.hide()
-                            scope.launch {
-                                snackbarHostState.showSnackbar(message = text, duration = SnackbarDuration.Short)
-                            }
-                        }) {
-                            Text("클릭!")
-                        }
-
-                    }
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "first") {
+                composable("first") {
+                    FirstScreen(navController)
                 }
-            )
+                composable("second") {
+                    SecondScreen(navController)
+                }
+                composable("third/{value}") {
+                    ThirdScreen(
+                        controller = navController,
+                        value = it.arguments?.getString("value") ?: ""
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FirstScreen(controller: NavController) {
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "첫 화면")
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            controller.navigate("second")
+        }) {
+            Text("두번째")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(value = value, onValueChange = setValue)
+
+        Button(onClick = {
+            if (value.isNotEmpty()) {
+                controller.navigate("third/$value")
+            }
+        }) {
+            Text("세번째")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(controller: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "두번째 화면")
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            controller.popBackStack()
+        }) {
+            Text("뒤로가기")
+        }
+    }
+}
+
+@Composable
+fun ThirdScreen(controller: NavController, value: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "세번째 화면")
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "value: $value")
+        Button(onClick = {
+            controller.popBackStack()
+        }) {
+            Text("뒤로가기")
         }
     }
 }
